591c32fad646684720e7cfe7b0c79b1a9da88aaa
HughParsonage <hugh.parsonage@gmail.com>
2019-02-09 00:08:01 +1100

    Fix underscores etc

--- tests/testthat/test_print_grouped_xtable.R
+++ tests/testthat/test_print_grouped_xtable.R
@@ -6,7 +6,10 @@ test_that("Returns expected output STE__Age_Sex", {
   expected <- readLines("print-grouped-xtable/STE__Age_Sex-expected.tex")
   print_grouped_xtable(DT,
                        group_by = c("STE_NAME16", "Age"),
-                       out.file = paste0("print-grouped-xtable/STE__Age_Sex-actual.tex"))
+                       align = "rrrr",
+                       out.file = paste0("print-grouped-xtable/STE__Age_Sex-actual.tex"),
+                       column_format = function(x) sprintf("\\textbf{%s}", 
+                                                           gsub("_", "\\\\_", x)))
   actual <- readLines("print-grouped-xtable/STE__Age_Sex-actual.tex")
   expect_equal(expected, actual)
 })
